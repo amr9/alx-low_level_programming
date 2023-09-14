185cb2e65f0cda24df8ef7fa4036a3f6cadac9e0
Amr Emad <amr12201147@gmail.com>
2023-09-14 12:50:42 -0700

    print triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - check for a digit
+ * @size : integer type
+ * Return:void
+ */
+
+void print_triangle(int size)
+{
+
+	int x = 1;
+	int y;
+
+	while (x <= size && size > 0)
+	{
+		y = 0;
+		while (y < size - x)
+		{
+			_putchar(' ');
+			y++;
+		}
+		y = 0;
+		while (ii < i)
+		{
+			_putchar('#');
+			y++;
+		}
+
+		_putchar('\n');
+		x++;
+	}
+	if (x == 1)
+		_putchar('\n');
+}

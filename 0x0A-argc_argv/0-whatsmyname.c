0b35b938422517876801c857a62ec11200b4aeac
Amr Emad <amr12201147@gmail.com>
2023-10-02 16:07:38 -0700

    betty errors

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - prints the name of the program
- *   * @argc: number of args
- *    * @argv: array of strings
- *     * Return: 0 - success
- *      */
+ * main - prints the name of the program
+ * @argc: number of args
+ * @argv: array of strings
+ * Return: 0 - success
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);

6e067c572d1e25500fdb2183048e3b235749ac19
Amr Emad <amr12201147@gmail.com>
2023-10-24 13:53:03 -0700

    error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,26 +1,20 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the nth node of a listint_t list.
- * @head: Pointer to the head of the list.
- * @index: The index of the node to retrieve, starting at 0.
- * Return: Pointer to the nth node, or NULL if the node does not exist.
+ * get_nodeint_at_index - returns the nth node of a linked list
+ * @head: head of a list.
+ * @index: index of the node.
+ *
+ * Return: nth node. If node does not exist, returns NULL.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (head != NULL)
+	for (i = 0; i < index && head != NULL; i++)
 	{
-		if (i == index)
-		{
-			return (head);
-		}
-
 		head = head->next;
-		i++;
 	}
 
-	return (NULL);
+	return (head);
 }
-

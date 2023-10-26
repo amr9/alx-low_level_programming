0e56b258d7fa56f5d0c8c0a9c76c990bde9eeab4
Amr Emad <amr12201147@gmail.com>
2023-10-26 15:03:12 -0700

    syntax errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,32 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
- *
- * Return: unsigned int.
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: Pointer to a string containing the binary number.
+ * Return: The converted number, or 0 .
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-	if (b != 0)
+	if (b == NULL)
 		return (0);
 
-	while (*b)
+	for (int i = 0; b[i] != '\0'; i++)
 	{
-		if (*b == '0' || *b == '1')
+		if (b[i] == '0')
 		{
-			result = (result << 1) | (*b - '0');
-			b++;
+			result = result << 1;
+		}
+		else if (b[i] == '1')
+		{
+			result = (result << 1) | 1;
 		}
 		else
+		{
 			return (0);
+		}
 	}
+
 	return (result);
 }

c9bc56cd76b78412889fef19e19f7b27b7a7fe1b
Amr Emad <amr12201147@gmail.com>
2023-10-26 15:00:10 -0700

    betty errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,23 @@
  *
  * Return: unsigned int.
  */
-unsigned int binary_to_uint(const char *b) {
-    unsigned int result = 0;
 
-    if (b != 0)
-        return 0;
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
 
-    while (*b) 
-    {
-        if (*b == '0' || *b == '1') 
-        {
-            result = (result << 1) | (*b - '0');
-            b++;
-        } 
-        else
-            return 0; 
-    }
-    return result;
+	if (b != 0)
+		return (0);
+
+	while (*b)
+	{
+		if (*b == '0' || *b == '1')
+		{
+			result = (result << 1) | (*b - '0');
+			b++;
+		}
+		else
+			return (0);
+	}
+	return (result);
 }
